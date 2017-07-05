f1ca58a813d5092b999f156878a7b23ba64c0eae
CryptoCoinUser <adkantor@gmail.com>
2017-07-05 13:35:08 -0700

    single page demo

--- js/index.js
+++ js/index.js
@@ -1,19 +1,74 @@
 require('babel-polyfill');
-
 import React from 'react';
 import ReactDOM  from 'react-dom';
 
-//import Person from './components/person';
-//import List from './components/list';
-import Board from './components/board';
+export class Listcontainer extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            changed: false,
+            text: "text placeholder",
+            cards: ['cards[] placeholder ', 'cards[] placeholder 2 ']
+        };
+        this.onAddInputChanged = this.onAddInputChanged.bind(this);
+    }
+
+    onAddInputChanged(){
+        this.setState({
+            changed: true
+        });
+    }
+
+    onFormSubmit(e, textInput){
+        e.preventDefault();
+        console.log(textInput);
+        this.setState({
+            cards: [...this.state.cards, textInput]
+        });
+    }
+
+    render() {
+
+        return (
+            <div className="listcontainer">
+                <List 
+                 cards={this.state.cards} 
+                 onAddInputChanged={this.onAddInputChanged} 
+                 onFormSubmit={this.onFormSubmit} 
+                />
+                <form id="addCard" onSubmit={(e) => this.onFormSubmit(e, this.textInput.value)}>
+                    <input 
+                        onChange={this.onAddInputChanged} 
+                        placeholder="new card text" 
+                        ref={(input) => this.textInput = input }
+                    />
+                    <button type="submit">Submit</button>
+                </form>
+            </div>
+        ); // return
+    } // render
+}
+
+export function List(props) {
+	const cards=props.cards.map(function(card, i){
+        return <Card text={card} key={i} />
+    })
+	return (
+        <div className="list">
+        	{cards} 
+        </div>
+    ); // return
+}
+
+export function Card(props){
+	return (
+		<div className="card">
+             <p>{props.text}</p>
+		</div>
+	); // return
+}
+
 
 document.addEventListener('DOMContentLoaded', () =>
-	//ReactDOM.render(<List />, document.getElementById('target'))
-    /* */
-    ReactDOM.render(<Board 
-    	board_title="Board title from index.js"
-    	board_lists={["list title 1 from index.js", "list title 2 from index.js", "list 3"]} 
-    	/>,
-     document.getElementById('target'))
-    
+    ReactDOM.render(<Listcontainer />, document.getElementById('target'))
 );
